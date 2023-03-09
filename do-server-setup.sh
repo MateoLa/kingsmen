977c0e501daff2b23b920aa502148c77d9185704
MateoLa <mateo.laino@gmail.com>
2023-03-09 18:58:49 -0300

    first

--- do-server-setup.sh
+++ do-server-setup.sh
@@ -1,44 +1,32 @@
 #!/bin/bash
 set -euo pipefail
 
-########################
-### SCRIPT VARIABLES ###
-########################
+### Variables
 
 # Name of the user to create and grant sudo privileges
 USERNAME=mateo
+HOSTNAME=$(curl -s http://169.254.169.254/metadata/v1/hostname)
+PUBLIC_IPV4=$(curl -s http://169.254.169.254/metadata/v1/interfaces/public/0/ipv4/address)
 
-# Whether to copy over the root user's `authorized_keys` file to the new sudo
-# user.
+# Whether to copy over the root `authorized_keys` file to the new user
 COPY_AUTHORIZED_KEYS_FROM_ROOT=true
 
-# Additional public keys to add to the new sudo user
+# Additional public keys to add to the new user
 OTHER_PUBLIC_KEYS_TO_ADD=(
-    "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDDGn+TCu5HKhdbMSWFBCGP2Nh9Hz9USmWhQ4ZdKcwRyqKm/6mnLIr3MBgZwvMJrMRSxaNEBX4PJcmO64RUoovyBcbjjjDLPjPNVHnG5ve2zJMJzYtzMahX6IxQl2S1xEjG4W1CStmCehyu2lhYdILGbm+HBJj4lMLWn5KVNrqiFNSWW7rpFgmKIF7u11qRUVgkzBUOle9aBvi2UkhkaBuABG3OTw3Jv2ROCdZtx/iA4rrwv7dyotLdMZWq4xbc0+LW3gYK0f/HJ/3ZYQywo8qgtSSR48W8682yUtwbVIEyPrl6cNsnaVnPcwiyaEmrynjCFGbVM+7mNa/BpmyIXmnRIVaaOayiZmc4UpjHVTjlU6zw7+p5SP8vY+AlQgk8UkddeoV404ilxbSerTwNvvzGQZpb7Q0wkm5kkm1tfcOqYuJ7ns3Nj3aaoIRioImQ81lBneVv9/i4JGd9WPe13utPHzBLLJTQm4dnvwSvNPor57JXfegyJ5/+S7C4avwdo1h0Lef5MGH6iprod2vQoDovYKyZVnyzu78FzepTDCa9tNGaGWfexQJKILHJpq9jLJoCuG2r4jlmThzm/Df2ede3hxvPT0EIHFvt+cnQkXTM+C+Rl/WT697Eeyb4Ka5IJZL8LIAmQHjHxlQWNFKy6SV5alQNfyAjChccMbSaMEb3BQ== ntdom1\c510268@u1704773"
 )
 
-####################
-### SCRIPT LOGIC ###
-####################
+### Script
+
+# Lock the root account to password-based access
+passwd --lock root
 
 # Add sudo user and grant privileges
-useradd --create-home --shell "/bin/bash" --groups sudo,docker "${USERNAME}"
+useradd --create-home --shell "/bin/bash" --groups sudo "${USERNAME}"
 
-# Check whether the root account has a real password set
-encrypted_root_pw="$(grep root /etc/shadow | cut --delimiter=: --fields=2)"
+# Remove the user's password so that a new password can be set without suppling a previous one
+passwd --delete "${USERNAME}"
 
-if [ "${encrypted_root_pw}" != "*" ]; then
-    # Transfer auto-generated root password to user if present
-    # and lock the root account to password-based access
-    echo "${USERNAME}:${encrypted_root_pw}" | chpasswd --encrypted
-    passwd --lock root
-else
-    # Delete invalid password for user if using keys so that a new password
-    # can be set without providing a previous value
-    passwd --delete "${USERNAME}"
-fi
-
-# Expire the sudo user's password immediately to force a change
+# Expire the user's password immediately to force a change
 chage --lastday 0 "${USERNAME}"
 
 # Create SSH directory for sudo user
@@ -61,11 +49,27 @@ chmod 0600 "${home_directory}/.ssh/authorized_keys"
 chown --recursive "${USERNAME}":"${USERNAME}" "${home_directory}/.ssh"
 
 # Disable root SSH login with password
-sed --in-place 's/^PermitRootLogin.*/PermitRootLogin prohibit-password/g' /etc/ssh/sshd_config
-if sshd -t -q; then
-    systemctl restart sshd
-fi
+# sed --in-place 's/^PermitRootLogin.*/PermitRootLogin prohibit-password/g' /etc/ssh/sshd_config
+# if sshd -t -q; then
+#    systemctl restart sshd
+# fi
+
+# Install nginx
+apt-get -y update
+apt-get -y install nginx
+echo Droplet: $HOSTNAME, IP Address: $PUBLIC_IPV4 > /var/www/html/index.html
 
 # Add exception for SSH and then enable UFW firewall
 ufw allow OpenSSH
+ufw allow 'Nginx Full'
 ufw --force enable
+
+# Pull Project
+Github_Origin="git@github.com:MateoLa/${HOSTNAME}.git"
+cd $home_directory
+mkdir $HOSTNAME
+cd $HOSTNAME
+git init
+git branch -m main
+git remote add origin $Github_Origin
+git pull origin main
